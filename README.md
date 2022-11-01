3e62830e2e89b7abd086f190b81bf78a56dfbaf6
Marat Yamaliev <marat.yamaliev@gmail.com>
2022-11-01 16:16:18 +0300

    make, linter, codeclimate

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/marat-y/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/marat-y/frontend-project-46/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/25013e623f19d88ff8f1/maintainability)](https://codeclimate.com/github/marat-y/frontend-project-46/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/25013e623f19d88ff8f1/test_coverage)](https://codeclimate.com/github/marat-y/frontend-project-46/test_coverage)
 
 ## Usage
 [Usage asciinema](https://asciinema.org/a/532434)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ import * as path from 'node:path';
 import { cwd } from 'node:process';
 
 import _ from 'lodash';
+
 const program = new Command();
 
 program
@@ -19,32 +20,34 @@ program
 program.parse();
 
 const [objectOne, objectTwo] = program.args
-                                      .map((fileName) => path.resolve(cwd(), fileName))
-                                      .map((filePath) =>  JSON.parse(fs.readFileSync(filePath, 'utf8')));
+  .map((fileName) => path.resolve(cwd(), fileName))
+  .map((filePath) => JSON.parse(fs.readFileSync(filePath, 'utf8')));
 
-const getDiff = (objectOne, objectTwo) => {
-  const changes = Object.keys(objectOne).reduce((acc, key) => {
-    if (!Object.hasOwn(objectTwo, key)) {
-      acc.push({ diff: '-', key: key, value: objectOne[key] });
-    } else if (objectOne[key] === objectTwo[key]) {
-      acc.push({ diff: ' ', key: key, value: objectOne[key] });
+const getDiff = (objOne, objTwo) => {
+  const changes = Object.keys(objOne).reduce((acc, key) => {
+    if (!Object.hasOwn(objTwo, key)) {
+      acc.push({ diff: '-', key, value: objOne[key] });
+    } else if (objOne[key] === objectTwo[key]) {
+      acc.push({ diff: ' ', key, value: objOne[key] });
     } else {
-      acc.push({ diff: '-', key: key, value: objectOne[key] });
-      acc.push({ diff: '+', key: key, value: objectTwo[key] });
+      acc.push({ diff: '-', key, value: objOne[key] });
+      acc.push({ diff: '+', key, value: objTwo[key] });
     }
 
     return acc;
   }, []);
-  
-  const newKeys = _.difference(Object.keys(objectTwo), Object.keys(objectOne));
-  for (const key of newKeys) {
-    changes.push({ diff: '+', key: key, value: objectTwo[key] });
-  }
+
+  const newKeys = _.difference(Object.keys(objTwo), Object.keys(objOne));
+
+  newKeys.reduce((acc, key) => {
+    acc.push({ diff: '+', key, value: objTwo[key] });
+    return acc;
+  }, changes);
 
   const sortedChanges = _.sortBy(changes, 'key');
   const content = sortedChanges.map((el) => ` ${el.diff} ${el.key}: ${el.value}`).join(', \n');
 
   return ['{', content, '}'].join('\n');
-}
+};
 
 console.log(getDiff(objectOne, objectTwo));
