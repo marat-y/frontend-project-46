573f5399f790e15460be65833d2fc97fb370b37a
Marat Yamaliev <marat.yamaliev@gmail.com>
2023-01-09 14:08:47 +0300

    readme update, cleanup

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-### Hexlet tests and linter status:
 [![Actions Status](https://github.com/marat-y/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/marat-y/frontend-project-46/actions)
 [![Node CI](https://github.com/marat-y/frontend-project-46/actions/workflows/nodejs.yml/badge.svg)](https://github.com/marat-y/frontend-project-46/actions/workflows/nodejs.yml)
 [![Maintainability](https://api.codeclimate.com/v1/badges/25013e623f19d88ff8f1/maintainability)](https://codeclimate.com/github/marat-y/frontend-project-46/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/25013e623f19d88ff8f1/test_coverage)](https://codeclimate.com/github/marat-y/frontend-project-46/test_coverage)
 
-## Usage
+gendiff.js - JSON and YML objects difference generator.
+
+### Usage
 - [flat JSON](https://asciinema.org/a/532434)
 - [flat YML](https://asciinema.org/a/535609)
 - [stylish nested](https://asciinema.org/a/541216)
--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,37 +9,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('stylish JSON', () => {
-  const expectedResult = readFile('stylishResult.txt').trim();
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'stylish')).toEqual(expectedResult);
-});
-
-test('stylish YML', () => {
-  const expectedResult = readFile('stylishResult.txt').trim();
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'stylish')).toEqual(expectedResult);
+test.each(['json', 'yml'])('gendiff %s', (objectFormat) => {
+  ['plain', 'json', 'stylish'].forEach((outputFormat) => {
+    const expectedResult = readFile(`${outputFormat}Result.txt`).trim();
+    expect(genDiff(`__fixtures__/file1.${objectFormat}`, `__fixtures__/file2.${objectFormat}`, outputFormat)).toEqual(expectedResult);
+  });
 });
 
 test('stylish as default', () => {
   const expectedResult = readFile('stylishResult.txt').trim();
   expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(expectedResult);
-});
-
-test('plain JSON', () => {
-  const expectedResult = readFile('plainResult.txt').trim();
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toEqual(expectedResult);
-});
-
-test('plain YML', () => {
-  const expectedResult = readFile('plainResult.txt').trim();
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain')).toEqual(expectedResult);
-});
-
-test('json JSON', () => {
-  const expectedResult = readFile('jsonResult.txt').trim();
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toEqual(expectedResult);
-});
-
-test('json YML', () => {
-  const expectedResult = readFile('jsonResult.txt').trim();
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json')).toEqual(expectedResult);
 });
